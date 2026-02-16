b608ebf7fbe84c974145f135105585add91a15eb
Manav <manavlathiya11@gmail.com>
2026-02-16 15:08:55 -0500

    add request models for vehicles api

---
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Models/MLCreateVehicleRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ML.VehicleInventory.WebAPI.Models
+{
+    public class MLCreateVehicleRequest
+    {
+        public string VehicleCode { get; set; } = string.Empty;
+        public int LocationId { get; set; }
+        public string VehicleType { get; set; } = string.Empty;
+    }
+}
---
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Models/MLUpdateVehicleStatusRequest.cs
@@ -0,0 +1,9 @@
+﻿using VehicleInventory.Domain.Enums;
+
+namespace ML.VehicleInventory.WebAPI.Models
+{
+    public class MLUpdateVehicleStatusRequest
+    {
+        public VehicleStatus Status { get; set; }
+    }
+}
