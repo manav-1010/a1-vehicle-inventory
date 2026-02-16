771d79a175ab18d263a630db3f708d53b8cf63df
Manav <manavlathiya11@gmail.com>
2026-02-16 15:06:17 -0500

    add vehicles controller skeleton

---
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Controllers/MLVehiclesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ML.VehicleInventory.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MLVehiclesController : ControllerBase
+    {
+    }
+}
