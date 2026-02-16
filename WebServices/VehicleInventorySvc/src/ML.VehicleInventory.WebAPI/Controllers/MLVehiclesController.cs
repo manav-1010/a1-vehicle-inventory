07c3bc4cc47a4211a61f7729820ab974ff441845
Manav <manavlathiya11@gmail.com>
2026-02-16 15:10:05 -0500

    setup vehicles controller route + service injection

--- WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Controllers/MLVehiclesController.cs
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Controllers/MLVehiclesController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ML.VehicleInventory.Application.Services;
 
 namespace ML.VehicleInventory.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/vehicles")]
     [ApiController]
     public class MLVehiclesController : ControllerBase
     {
+        private readonly MLVehicleService _vehicleService;
+
+        public MLVehiclesController(MLVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
     }
 }
