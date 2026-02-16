3d062549ba4f1aea82f7d18c7f9b5ef32cf53638
Manav <manavlathiya11@gmail.com>
2026-02-16 14:20:12 -0500

    add inventory dbcontext and EF packages

---
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.Infrastructure/Data/MLInventoryDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleInventory.Domain.Entities;
+
+namespace ML.VehicleInventory.Infrastructure.Data
+{
+    public class MLInventoryDbContext : DbContext
+    {
+        public MLInventoryDbContext(DbContextOptions<MLInventoryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> MLVehicles => Set<Vehicle>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>(entity =>
+            {
+                entity.ToTable("MLVehicles");
+
+                entity.HasKey(v => v.Id);
+
+                entity.Property(v => v.VehicleCode)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(v => v.VehicleType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(v => v.LocationId)
+                    .IsRequired();
+
+                // Status is enum: store as int by default
+                entity.Property(v => v.Status)
+                    .IsRequired();
+            });
+        }
+    }
+}
