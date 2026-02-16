3247fec767e19a82a0ed92622fcae2b890d62bd0
Manav <manavlathiya11@gmail.com>
2026-02-16 15:00:58 -0500

    created initial inventory migration

---
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.Infrastructure/Migrations/20260216200014_ML_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ML.VehicleInventory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ML_InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MLVehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleCode = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    VehicleType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MLVehicles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MLVehicles");
+        }
+    }
+}
