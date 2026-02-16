fde2995e980467094863dd687f49cf76a1f7fef9
Manav <manavlathiya11@gmail.com>
2026-02-16 14:51:34 -0500

    wired up dbcontext and DI in webapi

--- WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Program.cs
+++ WebServices/VehicleInventorySvc/src/ML.VehicleInventory.WebAPI/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ML.VehicleInventory.Application.Services;
+using ML.VehicleInventory.Application.Interfaces;
+using ML.VehicleInventory.Infrastructure.Data;
+using ML.VehicleInventory.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MLInventoryDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MLInventoryDb"));
+});
+
+builder.Services.AddScoped<IVehicleRepository, MLVehicleRepository>();
+builder.Services.AddScoped<MLVehicleService>();
+
 
 var app = builder.Build();
 
